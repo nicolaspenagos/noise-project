2570ba99a69ff3528816c58787bd82b6b11b9cf9
Nicolas <nicolas.penagosm98@gmail.com>
2020-04-23 00:34:14 -0500

    Second commit

--- scripts/Bird.js
+++ scripts/Bird.js
@@ -9,6 +9,8 @@ class Bird {
     this.color = color;
     this.selected = selected;
 
+    this.counter=1;
+
     this.count = Math.random() * 10000;
 
     this.radius = 150;
@@ -16,14 +18,36 @@ class Bird {
   }
 
   update() {
+    this.counter +=1;
     this.count += .01;
     const random = this.app.noise(this.count);
     const angle = random * Math.PI * 2;
 
     this.vel = p5.Vector.fromAngle(angle);
     this.vel.mult(5);
+    // M
+    if(this.pos.x<401&&this.pos.x>394&&this.pos.y<500&&this.pos.y>300){
 
-    this.pos.add(this.vel);
+    }else if(this.pos.x<415&&this.pos.x>385&&this.pos.y<310&&this.pos.y>300){
+
+    }else if(this.pos.x<415&&this.pos.x>385&&this.pos.y<500&&this.pos.y>490){
+
+    }else if(this.pos.x<1001&&this.pos.x>994&&this.pos.y<500&&this.pos.y>300){
+
+    }else if(this.pos.x<1001&&this.pos.x>920&&this.pos.y<500&&this.pos.y>490){
+
+    }else if(this.pos.x<910&&this.pos.x>905&&this.pos.y<500&&this.pos.y>300){
+    
+    }else{
+      this.pos.add(this.vel);
+    }
+
+    
+
+
+     
+    
+    
 
     const padding = 20;
     if(this.pos.x < -padding) this.pos.x = window.innerWidth + padding;
@@ -33,36 +57,26 @@ class Bird {
   }
 
   draw() {
-    const d = 10;
+
+    
+    const d = 5;
     this.app.push();
     this.app.translate(this.pos.x, this.pos.y);
     this.app.rotate(this.vel.heading() + Math.PI/2);
-    if(this.selected) {
-      this.app.fill(255, 30);
-      this.app.arc(0, 0, this.radius*2, this.radius*2, this.behindAngle/2 + Math.PI/2, Math.PI * 2.5 - this.behindAngle/2);
-      //this.app.ellipse(0, 0, this.radius);
-    }
+   
     this.app.fill(this.color);
-    this.app.triangle(
+   /* this.app.triangle(
       -d, d, // izquierda abajo
       d, d, // derecha abajo
       0, -d * 2,
-    );
+    );*/
+ 
+    this.app.stroke('white');
+    this.app.strokeWeight(3);
+    this.app.point(d,d);
     this.app.pop();
+    this.app.stroke('p');
 
-    this.birds.forEach(bird => {
-      if(bird === this) return;
-
-      const dist = this.pos.dist(bird.pos);
-      const angle = this.pos.angleBetween(bird.pos);
-      if(dist < this.radius){
-
-
-        if(this.selected){
-          this.app.stroke(this.color);
-          this.app.line(this.pos.x, this.pos.y, bird.pos.x, bird.pos.y);
-        }
-      }
-    });
+   
   }
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,35 @@
+
+
 function createSketch (app) {
 
   const birds = [];
+  
 
   app.setup = () => {
     app.createCanvas(window.innerWidth, window.innerHeight, p5.WEBGL);
 
-    for(let i = 0; i < 100; i++){
+    for(let i = 0; i < 1800; i++){
       let color = app.color(255);
-      if(i === 99) color = app.color(255, 0, 0);
-      birds.push(new Bird(app, birds, color, i === 99));
+      if(i === 599) color = app.color(255, 0, 0);
+      birds.push(new Bird(app, birds, color, i === 599));
     }
   }
   
 
   app.draw = () => {
-    app.background(0);
+    this.c+=1;
+
+    
+      app.background(0);
+
+      app.noStroke();
+  
+      app.fill(115,13,0);
+      app.ellipse((701*1.5)-350, 350*1.5-160, 60*1.5);
+      app.ellipse((650*1.5)-350, 350*1.5-160, 60*1.5);
+      app.triangle(621*1.5-350, 360*1.5-160, 730*1.5-350, 360*1.5-160, 673*1.5-350,435*1.5-160);
+    
+   
 
     birds.forEach(bird => {
       bird.update();
